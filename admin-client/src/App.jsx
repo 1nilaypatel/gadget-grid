ebbaa00f6ac9652a2a7d90b5200a993ba94324f2
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2023-12-20 12:42:50 +0530

    created pages and routes

--- admin-client/src/App.jsx
+++ admin-client/src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import React from 'react';
+import Home from './components/Home';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import About from './components/About';
+import Profile from './components/Profile';
 
 export default function App() {
-  return (
-    <h1 className='text-red-500'>App</h1>
-  )
+  return <div>
+    <Router>
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/sign-up"} element={<SignUp />} />
+        <Route path={"/sign-in"} element={<SignIn />} />
+        <Route path={"/about"} element={<About />} />
+        <Route path={"/profile"} element={<Profile />} />
+      </Routes>
+    </Router>
+  </div>
 }
