34184c22e0489558df9ed3166acab96857118060
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2023-12-22 00:42:24 +0530

    created sigin page ui

--- admin-client/src/pages/SignIn.jsx
+++ admin-client/src/pages/SignIn.jsx
@@ -1,7 +1,77 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch('/server/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
   return (
-    <div>SignIn</div>
-  )
+    <div className="p-6 max-w-lg mx-auto">
+      <h1 className='text-slate-100 text-center text-3xl font-semibold my-8'>Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+        <input 
+          type="email"
+          placeholder="email"
+          id="email"
+          className="border rounded-lg p-3"
+          onChange={handleChange}
+        />
+        <input 
+          type="password"
+          placeholder="password"
+          id="password"
+          className="border rounded-lg p-3"
+          onChange={handleChange}
+        />
+        <button 
+          disabled={loading}
+          className="bg-green-500 text-black p-3 rounded-lg uppercase hover:bg-opacity-85 disabled:bg-opacity-40"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-3 mt-5">
+        <p className="text-slate-200">New to GadgetGrid?</p>
+        <Link to={'/sign-up'}>
+          <span className="text-green-500">Sign Up</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-600 mt-5'>{error}</p>}
+    </div>
+  );
 }
