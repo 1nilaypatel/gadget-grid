da1946c46b98cf38cee92b967c56e6628ffdfd30
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2023-12-22 23:32:38 +0530

    updated Appbar, made profile page private

--- admin-client/src/App.jsx
+++ admin-client/src/App.jsx
@@ -1,11 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import {FaSearch} from 'react-icons/fa';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Appbar from './components/Appbar';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return <div>
@@ -16,7 +16,9 @@ export default function App() {
         <Route path={"/sign-up"} element={<SignUp />} />
         <Route path={"/sign-in"} element={<SignIn />} />
         <Route path={"/about"} element={<About />} />
-        <Route path={"/profile"} element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path={"/profile"} element={<Profile />} />
+        </Route>
       </Routes>
     </Router>
   </div>
--- admin-client/src/components/Appbar.jsx
+++ admin-client/src/components/Appbar.jsx
@@ -1,7 +1,10 @@
 import { FaSearch } from 'react-icons/fa';
+import { useSelector } from 'react-redux';
 import {Link} from 'react-router-dom';
 
 export default function Appbar() {
+  const {currentUser} = useSelector((state) => state.user);
+
   return (
     <header className='bg-black shadow-md'>
       <div className='flex justify-between items-center p-3 max-w-6xl mx-auto'>
@@ -32,10 +35,13 @@ export default function Appbar() {
               About
             </li>
           </Link>
-          <Link to='/sign-in'>
-          <li className='text-slate-400 hover:underline'>
-              SignIn
-            </li>
+
+          <Link to='/profile'>
+            {currentUser ?(
+              <img className='rounded-full h-8 w-8 object-cover' src={currentUser.profilephoto} alt='profile' />
+            ): (
+              <li className='text-slate-400 hover:underline'> SignIn </li>
+            )}
           </Link>
         </ul>
       </div>
