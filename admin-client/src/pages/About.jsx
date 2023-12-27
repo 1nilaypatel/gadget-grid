18388a113d925770b3022ffa248d3f137e78c1d0
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2023-12-27 17:59:37 +0530

    completed About page

--- admin-client/src/pages/About.jsx
+++ admin-client/src/pages/About.jsx
@@ -2,6 +2,22 @@
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4'>
+        <span className='text-slate-100'>Gadget</span>
+        <span className='text-slate-400'>Grid</span>
+      </h1>
+      <h1 className='text-xl font-bold mb-4 text-slate-400'>Embark on a delightful journey with GadgetGrid, <br/> where we redefine the way you experience music, gaming, and beyond.
+      </h1>
+      <p className='mb-4 text-slate-300'>
+      Welcome to GadgetGrid – your ultimate destination for curated headphones and earphones, tailored for the new generation's immersive music, gaming, and lifestyle experiences. At GadgetGrid, we take pride in handpicking a diverse selection of audio devices, ensuring our customers discover the perfect blend of style, performance, and innovation.
+      </p>
+      <p className='mb-4 text-slate-300'>
+        Our mission is to bridge the gap between enthusiasts and top-tier sellers, providing a seamless platform for discovering and acquiring premium audio solutions. Whether you're on the hunt for the latest trends, exceptional sound quality, or cutting-edge features, GadgetGrid is your go-to source for elevating your auditory experience.
+      </p>
+      <p className='mb-4 text-slate-300'>
+        Our dedicated team understands the importance of finding the right audio companion, and we're here to guide you through this exciting journey. Explore our curated collection, immerse yourself in the world of superior sound, and let GadgetGrid connect you with sellers who share our passion for delivering quality and satisfaction.
+      </p>
+    </div>
   )
 }
