c28ea7e4a64b9125995da8c88a5ce94c5ff4360a
1nilaypatel <nilaynilaypatelpatel@gmail.com>
2023-12-27 07:18:27 +0530

    created listing items, show listing, show more listing

--- admin-client/src/pages/Search.jsx
+++ admin-client/src/pages/Search.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import ListingItem from '../components/ListingItem';
 
 export default function Search() {
   const navigate = useNavigate();
@@ -13,7 +14,7 @@ export default function Search() {
 
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
-  console.log(listings);
+  const [showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -35,9 +36,15 @@ export default function Search() {
 
     const fetchListings = async () => {
       setLoading(true);
+      setShowMore(false);
       const searchQuery = urlParams.toString();
       const response = await fetch(`/server/listing/get?${searchQuery}`);
       const data = await response.json();
+      if (data.length > 8) {
+        setShowMore(true);
+      } else {
+        setShowMore(false);
+      }
       setListings(data);
       setLoading(false);
     };
@@ -72,6 +79,20 @@ export default function Search() {
     urlParams.set('order', sidebardata.order);
     const searchQuery = urlParams.toString();
     navigate(`/search?${searchQuery}`);
+  };
+
+  const onShowMoreClick = async () => {
+    const numberOfListings = listings.length;
+    const startIndex = numberOfListings;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', startIndex);
+    const searchQuery = urlParams.toString();
+    const response = await fetch(`/api/listing/get?${searchQuery}`);
+    const data = await response.json();
+    if (data.length < 9) {
+      setShowMore(false);
+    }
+    setListings([...listings, ...data]);
   };
 
   return (
@@ -120,8 +141,31 @@ export default function Search() {
           </button>
         </form>
       </div>
-      <div className=''>
-        <h1 className='text-3xl font-semibold border-b border-slate-700 p-3 mt-5'>Look what we found:</h1>
+      <div className='flex-1'>
+        <h1 className='text-3xl font-semibold border-b border-slate-700 p-3 mt-5'>
+          Look what we found:
+        </h1>
+        <div className='p-4 flex flex-wrap gap-4'>
+          {!loading && listings.length === 0 && (
+            <p className='text-xl text-slate-300'>No Gadgets in the Grid!</p>
+          )}
+          {loading && (
+            <p className='text-xl text-slate-300 text-center w-full'>
+              Loading...
+            </p>
+          )}
+          {!loading && listings && listings.map((listing) => (
+              <ListingItem key={listing._id} listing={listing} />
+            ))}
+            {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className='text-green-700 hover:underline p-7 text-center w-full'
+            >
+              Show more
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
